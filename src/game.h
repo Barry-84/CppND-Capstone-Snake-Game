fa7006d63ac3e3e12be20716223e322ab59af388
Barry-84 <barrydol@gmail.com>
2023-02-09 09:55:33 +0000

    Fixed compile error

--- src/game.h
+++ src/game.h
@@ -9,9 +9,7 @@
 
 class Game {
  public:
-  //Game(std::size_t grid_width, std::size_t grid_height);
-  Game(std::size_t grid_width, std::size_t grid_height)
-     : score{0} {}
+  Game(std::size_t grid_width, std::size_t grid_height);
   void Run(Controller const &controller, Renderer &renderer,
            std::size_t target_frame_duration);
   int GetScore() const;
@@ -26,7 +24,6 @@ class Game {
   std::uniform_int_distribution<int> random_w;
   std::uniform_int_distribution<int> random_h;
 
-  //int score{0};
   int score;
 
   void PlaceFood();
